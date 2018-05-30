d015ebfc6bffef4b4b5539a70ef4ff850a6bc85f
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-31 00:28:28 +0500

    Updated Menu to Simple function call

--- SchoolSystem/Source.cpp
+++ SchoolSystem/Source.cpp
@@ -10,41 +10,7 @@ int main()
 
     Admin admin;
 
-    for (char ch = NULL; ch != 'c' || ch != 'C'; ch = admin.panel(ch))
-    {
-        switch (ch)
-        {
-        case 'a':
-        case 'A':
-            admin.addClass();
-            break;
-
-        case 'r':
-        case 'R':
-            admin.removeClass();
-            break;
-
-        case 'e':
-        case 'E':
-            admin.enterClass();
-            break;
-
-        case 'l':
-        case 'L':
-            admin.listClasses();
-            break;
-
-        case 'g':
-        case 'G':
-            admin.listLogs();
-            break;
-
-        default:            
-            if (ch == NULL) continue;         
-            cout << " >> Wrong choice!" << endl;
-            admin.pushLog("Wrong Choice");
-        }
-    }
+    admin.panel();
 
     system("pause");
     return 0;
