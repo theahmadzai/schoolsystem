a70b648888420e8933c9ae956a5e02a3a84b7c87
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-05-31 00:27:28 +0500

    Fixed

--- SchoolSystem/Admin.cpp
+++ SchoolSystem/Admin.cpp
@@ -1,73 +1,166 @@
 #include "Admin.h"
 
-Admin::Admin()
+Admin::Admin(): logger(new Logger)
 {
+    readData();
 }
 
 Admin::~Admin()
 {
+    delete current;
+    delete logger;
+}
+
+void Admin::panel()
+{
+    char ch = NULL;
+    do { 
+        cout << endl << endl;
+        cout << "\t\t\t+---------------------------------+" << endl;
+        cout << "\t\t\t|                                 |" << endl;
+        cout << "\t\t\t|       C  -  QUIT                |" << endl;
+        cout << "\t\t\t|       A  -  ADD CLASS           |" << endl;
+        cout << "\t\t\t|       R  -  REMOVE CLASS        |" << endl;
+        cout << "\t\t\t|       E  -  ENTER CLASS         |" << endl;
+        cout << "\t\t\t|       L  -  LIST CLASSES (" << totalClasses() << ")\t  |" << endl;
+        cout << "\t\t\t|       G  -  LIST LOGS (" << logger->totalLogs() << ")\t  |" << endl;
+        cout << "\t\t\t|                                 |" << endl;
+        cout << "\t\t\t+---------------------------------+" << endl;
+        cout << "\t\t\t| Last Action: " << logger->getLastLog() << endl;
+        cout << "\t\t\t+----------------------------------" << endl;
+        cout << "\t\t\t>> "; cin >> ch;
+
+        system("cls");
+
+        switch (ch)
+        {
+        case 'a':
+        case 'A':
+            addClass();
+            break;
+
+        case 'r':
+        case 'R':
+            removeClass();
+            break;
+
+        case 'e':
+        case 'E':
+            enterClass();
+            break;
+
+        case 'l':
+        case 'L':
+            listClasses();
+            break;
+
+        case 'g':
+        case 'G':
+            logger->listLogs();
+            break;
+
+        default:
+            if (ch == NULL) continue;
+            cout << " >> Wrong choice!" << endl;
+            logger->pushLog("Wrong Choice");
+        }
+
+    } while (ch != 'c' && ch != 'C');
 }
 
 void Admin::addClass()
 {
-    system("cls");
-    string name;
+    char name[10];
     cout << " >> Enter class name: "; cin >> name;
     unsigned int fee;
     cout << " >> Enter class fee: "; cin >> fee;
+
     Class temp(name, fee);
-    classes.emplace_back(temp);
-    system("cls");
-    cout << " --> CLASS ADDED SUCCESSFULLY!" << endl;
-    pushLog("Added Class " + name);
+    classes.push_back(temp);
+
+    writeData(temp);
+
+    notify("Added class: " + string(name));
 }
 
 void Admin::removeClass()
 {
-    
+    unsigned int cid;
+    cout << "Enter class id to remove: "; cin >> cid;
+
+    if(!existClass(cid)){
+        cout << "Class id: [" << cid  << "] not found!" << endl;
+        return;
+    }
+
+    classes.erase(classes.begin() + getClassIndex(cid));
+
+    rewriteData();
+
+    notify("Removed class: " + to_string(cid));
 }
 
 void Admin::listClasses()
 {
-    if (classes.size() < 1) return;
+    if (totalClasses() < 1) return;
+
+    notify("Listed classes: " + to_string(totalClasses()));
+
     cout << "+---------------+---------------+---------------+---------------+---------------+---------------+" << endl;
     cout << "| CLASS         | NAME          | STUDENTS      | TEACHERS      | PER FEE       | TOTAL FEE     |" << endl;
     cout << "+---------------+---------------+---------------+---------------+---------------+---------------+" << endl;
-    for (int i = 0; i < classes.size(); i++) {
-        classes[i].info();
+    for (int i = 0; i < totalClasses(); i++) {
+        cout << "| " << classes[i].getId() << " \t\t| " << classes[i].getName() << " \t\t| " << classes[i].getTotalStudents() << " \t\t| " << classes[i].getTotalTeachers() << " \t\t| " << classes[i].getFee() << " \t\t| " << classes[i].getTotalFee() << " \t\t|" << endl;
     }
-    pushLog("Listed Classes");
+    cout << "+---------------+---------------+---------------+---------------+---------------+---------------+" << endl;    
 }
 
 void Admin::enterClass()
 {
-    int c;
-    cout << " >> Class ID: "; cin >> c;
+    int cid;
+    cout << " >> Class ID: "; cin >> cid;
 
-    if (classes.size() < 1) {
-        cout << "Enter classes first!" << endl;
+    if (totalClasses() < 1) {
+        cout << "Empty classes!" << endl;
         return;
     }
+
+    if (!existClass(cid)) {
+        cout << "Class id [" << cid << "] not found!" << endl;
+        return;
+    }
+
+    current = &classes[getClassIndex(cid)];
+
+    notify("Entered class: " + to_string(cid));
+
+    current->panel();
+}
+
+bool Admin::existClass(unsigned int _id)
+{
     for (int i = 0; i < classes.size(); i++) {
-        if (classes[i].getId() == c) {
-            c = i;
-            break;
-        }
-        if (classes[i].getId() != c && i == classes.size() - 1) {
-            cout << "Class " << c << " not found!" << endl;
-            return;
+        if (classes[i].getId() == _id) {
+            return true;
         }
     }
-    cout << "INDEX: "<< c << endl;
-    cout << "VALUE: "<< classes[c].getId() << endl;
-    *current = classes[c];
+    return false;
+}
 
-    cout << "Entered class " << current->getId() << endl;
+int Admin::getClassIndex(unsigned int _id)
+{
+    unsigned int index = NULL;
 
-    for (char ch = NULL; ch != 'b'; ch = this->current->panel(ch))
-    {
-      
+    for (int i = 0; i < classes.size(); i++) {
+        for (int i = 0; i < classes.size(); i++) {
+            if (classes[i].getId() == _id) {
+                index = i;
+                break;
+            }
+        }
     }
+
+    return index;
 }
 
 int Admin::totalClasses()
@@ -75,53 +168,65 @@ int Admin::totalClasses()
     return classes.size();
 }
 
-int Admin::totalLogs()
+void Admin::notify(string txt)
 {
-    return logs.size();
-}
-
-char Admin::panel(char ch)
-{
-    cout << endl << endl;
-    cout << "\t\t\t+---------------------------------+" << endl;
-    cout << "\t\t\t|                                 |" << endl;
-    cout << "\t\t\t|       A  -  ADD CLASS           |" << endl;
-    cout << "\t\t\t|       R  -  REMOVE CLASS        |" << endl;
-    cout << "\t\t\t|       E  -  ENTER CLASS         |" << endl;
-    cout << "\t\t\t|       L  -  LIST CLASSES (" << totalClasses() << ")\t  |" << endl;
-    cout << "\t\t\t|       G  -  LIST LOGS (" << totalLogs() << ")\t  |" << endl;
-    cout << "\t\t\t|                                 |" << endl;
-    cout << "\t\t\t+---------------------------------+" << endl;
-    cout << "\t\t\t| Last Action: " << getLastLog() << endl;
-    cout << "\t\t\t+----------------------------------" << endl;
-    cout << "\t\t\t>> "; cin >> ch;
+    logger->pushLog(txt);
 
     system("cls");
-    return ch;
+    cout << "**************************************************" << endl;
+    cout << " --> " + txt << endl;
+    cout << "**************************************************" << endl;
 }
 
-void Admin::pushLog(string text) {
-    logs.push_back(getCurrentTime() + " - " + text);
+void Admin::writeData(Class &c)
+{
+    ofstream file;
+
+    file.open("storage/classes.dat", ios::binary | ios::out | ios::app);
+
+    file.write((char*)&c, sizeof(Class));
+
+    file.close();
 }
 
-void Admin::listLogs() {
-    for (int i = 0; i < logs.size(); i++) {
-        cout << i+1 << " - " << logs[i] << endl;
+void Admin::rewriteData()
+{
+    ofstream file;
+
+    file.open("storage/classes.dat", ios::binary | ios::out);
+
+    for (int i = 0; i < classes.size(); i++) {
+        file.write((char*)&classes[i], sizeof(Class));
     }
-    pushLog("Listed Logs");
+
+    file.close();
 }
 
-string Admin::getLastLog() {
-    if (logs.size() < 1) {
-        return "No Actions Peformed Yet!";
+void Admin::readData()
+{
+    ifstream file;
+
+    file.open("storage/classes.dat", ios::binary | ios::in);
+
+    if (!file.is_open()) {
+        return;
     }
-    return logs[logs.size() - 1];
-}
 
-string Admin::getCurrentTime() {
-    return (
-        "[" + to_string(((time(0) / 60 / 60) - 6) % 24)
-        + ":" + to_string((time(0) / 60) % 60)
-        + ":" + to_string((time(0)) % 60) + "]"
-    );
+    Class c;
+
+    while (file.read((char*)&c, sizeof(Class)))
+    {
+        classes.push_back(c);
+    }
+
+    int prev = classes[0].getId();
+    int tid = prev;
+    for (int i = 0; i < classes.size(); i++) {
+        if (classes[i].getId() > prev) {
+            tid = classes[i].getId();
+        }
+    }
+    Class::setTotalClasses(tid);
+
+    file.close();
 }
